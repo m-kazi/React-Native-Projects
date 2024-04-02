8255b81a1a573e00f69e3f4a032683f959792804
m-kazi <kazzcode@gmail.com>
2024-04-02 23:18:10 +0600

    Fancy Cards Part-01

--- STYLER02/App.tsx
+++ STYLER02/App.tsx
@@ -1,7 +1,8 @@
-import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import {SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
 import FlatCards from './components/FlatCards';
 import ElevatedCards from './components/ElevatedCards';
+import FancyCards from './components/FancyCards';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <ScrollView>
         <FlatCards />
         <ElevatedCards />
+        <FancyCards />
       </ScrollView>
     </SafeAreaView>
   );
---
+++ STYLER02/components/FancyCards.tsx
@@ -0,0 +1,68 @@
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function FancyCards() {
+  return (
+    <View>
+      <Text style={styles.headingText}>Trending Places</Text>
+      <View style={[styles.card, styles.cardElevated]}>
+        <Image
+          source={{
+            uri: 'https://images.pexels.com/photos/208745/pexels-photo-208745.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+          }}
+          style={styles.cardImage}
+        />
+        <View style={styles.cardBody}>
+          <Text style={styles.cardTitle}>Golden Gate</Text>
+          <Text style={styles.cardLabel}>Golden Gate Bridge</Text>
+          <Text style={styles.cardDescription}>
+            The Golden Gate Bridge is a suspension bridge spanning the Golden
+            Gate, the one-mile-wide strait connecting San Francisco Bay and the
+            Pacific Ocean.
+          </Text>
+          <Text style={styles.cardFooter}>Golden Gate, California</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    paddingHorizontal: 10,
+    textAlign: 'center',
+  },
+  card: {
+    width: 340,
+    height: 300,
+    marginVertical: 15,
+    marginHorizontal: 10,
+    padding: 2,
+    borderRadius: 5,
+  },
+  cardElevated: {
+    backgroundColor: '#CAD5E2',
+    elevation: 5,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+  },
+  cardImage: {
+    height: 160,
+    borderTopRightRadius: 5,
+    borderTopLeftRadius: 5,
+    marginBottom: 5,
+  },
+  cardBody: {
+    flex: 1,
+    flexGrow: 1,
+    paddingHorizontal: 10,
+  },
+  cardTitle: {},
+  cardLabel: {},
+  cardDescription: {},
+  cardFooter: {},
+});
