7d52e74e24c3aaecb448db95fd5e6c285a8e98da
m-kazi <kazzcode@gmail.com>
2024-04-23 15:55:17 +0600

    Roll The Dice Completed

--- rollTheDice05/src/App.tsx
+++ rollTheDice05/src/App.tsx
@@ -5,14 +5,79 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Image,
+  ImageSourcePropType,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
-import {StyleSheet, Text, View} from 'react-native';
+// importing images & type declaration for TS
+import DiceOne from '../assets/One.png';
+import DiceTwo from '../assets/Two.png';
+import DiceThree from '../assets/Three.png';
+import DiceFour from '../assets/Four.png';
+import DiceFive from '../assets/Five.png';
+import DiceSix from '../assets/Six.png';
 
-function App(): React.JSX.Element {
+type DiceProps = PropsWithChildren<{
+  imageUrl: ImageSourcePropType;
+}>;
+
+// image componenet
+const Dice = ({imageUrl}: DiceProps): JSX.Element => {
   return (
     <View>
-      <Text>Text is here</Text>
+      <Image style={styles.diceImage} source={imageUrl} />
+    </View>
+  );
+};
+
+function App(): React.JSX.Element {
+  // state management
+  const [diceImage, setDiceImage] = useState<ImageSourcePropType>(DiceOne);
+
+  // function for random images
+  const rollTheDice = () => {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+
+    switch (randomNumber) {
+      case 1:
+        setDiceImage(DiceOne);
+        break;
+      case 2:
+        setDiceImage(DiceTwo);
+        break;
+      case 3:
+        setDiceImage(DiceThree);
+        break;
+      case 4:
+        setDiceImage(DiceFour);
+        break;
+      case 5:
+        setDiceImage(DiceFive);
+        break;
+      case 6:
+        setDiceImage(DiceSix);
+        break;
+
+      default:
+        setDiceImage(DiceOne);
+
+        break;
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Dice imageUrl={diceImage} />
+      <Pressable onPress={rollTheDice}>
+        <Text style={styles.rollDiceBtnText}>Roll The Dice</Text>
+      </Pressable>
     </View>
   );
 }
@@ -22,7 +87,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#FFF2F2',
+    backgroundColor: '#000000',
   },
   diceContainer: {
     margin: 12,
